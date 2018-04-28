f15d1716db5a8de5afafa3249cd8e66fa74a279e
berat <berat.civgin@outlook.com>
2018-04-28 14:31:46 +0300

    es6 features

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,6 +1,7 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
 
     $scope.messages = [ ];
+    $scope.players = { };
 
     $scope.init = () => {
         const username = prompt('Please enter username');
@@ -11,46 +12,119 @@ app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFacto
             return false;
     };
 
+    function scrollTop() {
+        setTimeout(() => {
+            const element = document.getElementById('chat-area');
+            element.scrollTop = element.scrollHeight;
+        });
+    }
 
-    function initSocket(username) {
+    function showBubble(id, message) {
+        $('#'+ id).find('.message').show().html(message);
+
+        setTimeout(() => {
+            $('#'+ id).find('.message').hide();
+        }, 2000);
+    }
+
+    // noinspection JSAnnotator
+    // noinspection JSAnnotator
+    async function initSocket(username) {
         const connectionOptions = {
             reconnectionAttempts: 3,
             reconnectionDelay: 600
         };
 
-      indexFactory.connectSocket('http://localhost:3000', connectionOptions)
-          .then((socket) => {
+        try{
+            const socket = await indexFactory.connectSocket('http://localhost:3000', connectionOptions);
+
             socket.emit('newUser', { username });
+
+            socket.on('initPlayers', (players) => {
+                $scope.players = players;
+                $scope.$apply();
+            });
 
             socket.on('newUser', (data) => {
                 const messageData = {
                     type: {
                         code: 0, // server or user message
                         message: 1 // login or disconnect message
-                      }, // info
-                      username: data.username,
+                    }, // info
+                    username: data.username,
 
-                  };
+                };
 
-                  $scope.messages.push(messageData);
-                  $scope.$apply();
-              });
+                $scope.messages.push(messageData);
+                $scope.players[data.id] = data;
+                scrollTop();
+                $scope.$apply();
+            });
 
-              socket.on('disUser', (data) => {
-                  const messageData = {
-                      type: {
-                          code: 0,
-                          message: 0
-                      }, // info
-                      username: data.username
-                  };
+            socket.on('disUser', (data) => {
+                const messageData = {
+                    type: {
+                        code: 0,
+                        message: 0
+                    }, // info
+                    username: data.username
+                };
 
-                  $scope.messages.push(messageData);
-                  $scope.$apply();
-              });
-          }).catch((err) => {
+                $scope.messages.push(messageData);
+                delete $scope.players[data.id];
+                scrollTop();
+                $scope.$apply();
+            });
+
+            socket.on('animate', data => {
+                $('#'+ data.socketId).animate({ 'left': data.x, 'top': data.y }, () => {
+                    animate = false;
+                });
+            });
+
+            socket.on('newMessage', message => {
+                $scope.messages.push(message);
+                $scope.$apply();
+                showBubble(message.socketId, message.text)
+                scrollTop();
+            });
+
+            let animate = false;
+            $scope.onClickPlayer = ($event) => {
+                if (!animate){
+                    let x = $event.offsetX;
+                    let y = $event.offsetY;
+
+                    socket.emit('animate', { x, y });
+
+                    animate = true;
+                    $('#'+ socket.id).animate({ 'left': x, 'top': y }, () => {
+                        animate = false;
+                    });
+                }
+            };
+
+            $scope.newMessage = () =>{
+                let message = $scope.message;
+
+                const messageData = {
+                    type: {
+                        code: 1, // server or user message
+                    },
+                    username: username,
+                    text: message
+                };
+
+                $scope.messages.push(messageData);
+                $scope.message = '';
+
+                socket.emit('newMessage', messageData);
+
+                showBubble(socket.id, message);
+                scrollTop();
+            };
+        }catch(err){
             console.log(err);
-          });
+        }
     }
-
 }]);
--- src/socketApi.js
+++ src/socketApi.js
@@ -6,6 +6,9 @@ socketApi.io = io;
 
 const users = { };
 
+//helpers
+const randomColor = require('../helpers/randomColor');
+
 io.on('connection', (socket) => {
     console.log('a user connected');
 
@@ -15,21 +18,42 @@ io.on('connection', (socket) => {
             position: {
                 x: 0,
                 y: 0
-            }
+            },
+            color: randomColor()
         };
 
         const userData = Object.assign(data, defaultData);
-        users[socketio.id] = userData;
+        users[socket.id] = userData;
         console.log(users);
 
         socket.broadcast.emit('newUser', users[socket.id]);
+        socket.emit('initPlayers', users);
     });
 
     socket.on('disconnect', () => {
         socket.broadcast.emit('disUser', users[socket.id]);
         delete users[socket.id];
+    });
 
-        console.log(users);
+    socket.on('animate', (data) => {
+        try {
+            users[socket.id].position.x = data.x;
+            users[socket.id].position.y = data.y;
+
+            socket.broadcast.emit('animate', {
+                socketId: socket.id,
+                x: data.x,
+                y: data.y
+            });
+        }catch (e) {
+            console.log(e);
+        }
+
+    });
+
+    socket.on('newMessage', data => {
+        const messageData = Object.assign({socketId: socket.id}, data);
+        socket.broadcast.emit('newMessage', messageData);
     });
 
 });
